b048d306c98f452dbf29fe966ecaf1bbb9464571
Haixiang <haixiang6123@gmail.com>
2020-05-06 12:56:43 -0700

    添加 puppeteer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('start')
+import puppeteer from 'puppeteer'
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://www.1point3acres.com/bbs/');
+
+  await page.click('')
+
+  await browser.close();
+})();

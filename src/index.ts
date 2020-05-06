5b06ff87ee15c53d7e22e2d154debc318bf2d9c6
Haixiang <haixiang6123@gmail.com>
2020-05-06 14:00:18 -0700

    添加登录功能

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import puppeteer from 'puppeteer'
+import puppeteer, {Page} from 'puppeteer'
+import {loginButton, passwordInput, usernameInput} from './lib/selectors'
+import {sleep} from './lib/tool'
+
+const login = async (page: Page, username: string, password: string) => {
+  await page.focus(usernameInput)
+  await page.keyboard.type(username)
+
+  await page.focus(passwordInput)
+  await page.keyboard.type(password)
+
+  await page.click(loginButton)
+}
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-  await page.goto('https://www.1point3acres.com/bbs/');
+  await page.goto('https://www.1point3acres.com/bbs/')
 
-  await page.click('')
+  await login(page, 'username', 'password')
 
-  await browser.close();
-})();
+  await sleep(3000)
+
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close()
+})()
---
+++ src/lib/selectors.ts
@@ -0,0 +1,4 @@
+// 登录
+export const usernameInput = '#ls_username'
+export const passwordInput = '#ls_password'
+export const loginButton = '#lsform > div > div.y.pns > table > tbody > tr:nth-child(3) > td:nth-child(2) > button'

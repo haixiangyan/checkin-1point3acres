7698769d9e09473700b3af5870d03c9455882727
Haixiang <haixiang6123@gmail.com>
2020-05-07 11:43:21 -0700

    使用 password 输入

--- constants/questions.ts
+++ constants/questions.ts
@@ -1,5 +1,5 @@
 export const userQuestion = [
   {type: 'input', name: 'username', message: '一亩三分地的账号'},
-  {type: 'input', name: 'password', message: '一亩三分地的密码'},
+  {type: 'password', name: 'password', message: '一亩三分地的密码'},
   {type: 'confirm', name: 'saveCache', message: '是否缓存到本地'},
 ]

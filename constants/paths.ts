48d4ee35d92f2cbf956b1e0e8dd234e6c7347361
Haixiang <haixiang6123@gmail.com>
2020-05-07 12:10:47 -0700

    去掉 dotenv

--- constants/paths.ts
+++ constants/paths.ts
@@ -1,4 +1,3 @@
 import * as path from 'path'
 
 export const userPath = path.resolve(__dirname, '../db/user.json')
-export const dotenvPath = path.resolve(__dirname, '../.env')
--- tests/logic/auth.spec.ts
+++ tests/logic/auth.spec.ts
@@ -1,7 +1,6 @@
-import * as fs from 'fs'
 import * as inquirer from 'inquirer'
 import {getUser} from '../../logic/auth'
-import {dotenvPath, userPath} from '../../constants/paths'
+import {userPath} from '../../constants/paths'
 import {clearUserCache, readJSONSync, writeJSONSync} from '../../lib/tool'
 
 jest.mock('inquirer')
@@ -14,20 +13,6 @@ afterAll(() => {
 })
 
 describe('getUser', () => {
-  it('获取测试用户', async () => {
-    process.env.NODE_ENV = 'development'
-
-    const user = await getUser()
-
-    // CI 上不测试
-    if (!fs.existsSync(dotenvPath)) return
-
-    const raw = fs.readFileSync(dotenvPath, 'utf8')!
-    const [rawUsername, rawPassword] = raw.split('\n')
-
-    expect(user.username).toEqual(rawUsername.split('=')[1])
-    expect(user.password).toEqual(rawPassword.split('=')[1])
-  })
   it('获取缓存的用户', async () => {
     process.env.NODE_ENV = 'production'
 
